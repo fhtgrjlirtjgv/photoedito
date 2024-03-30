b8227e2a46a0d4ccf6065f0b94151ed9a520b95d
fhtgrjlirtjgv <154062090+fhtgrjlirtjgv@users.noreply.github.com>
2024-03-30 18:24:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from PIL import Image, ImageFilter
+from PyQt5.QtCore import Qt
+from PIL import Image , ImageFilter
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QFileDialog , QMessageBox
+from PyQt5.QtGui import QPixmap 
 from ui import Ui_MainWindow
-
 import os
 
 class Widget(QMainWindow):
@@ -14,7 +15,8 @@ class Widget(QMainWindow):
         self.filenames = None
         self.image = None
         self.ui.papka_button.clicked.connect(self.choose_folder)
-
+        self.ui.bw_pic.clicked.connect(self.do_black_white)
+        self.ui.image_list.currentRowChanged.connect(self.show_chosen_image)
     def show_image_list(self):
         self.filenames = os.listdir(self.workdir)
         images = []
@@ -32,6 +34,47 @@ class Widget(QMainWindow):
             message = QMessageBox()
             message.setText("ERROR")
             message.exec_()
+
+    def load_image(self, imagename):
+        self.image_name = imagename
+        self.image_path = os.path.join(self.workdir, self.image_name)
+        self.image = Image.open(self.image_path)
+
+
+    def show_image(self):
+        self.ui.image_label.hide()
+        h = self.ui.image_label.height()
+        w = self.ui.image_label.width()
+
+        pixmap_image = QPixmap(self.image_path)
+        pixmap_image = pixmap_image.scaled(w, h, Qt.KeepAspectRatio)
+        self.ui.image_label.setPixmap(pixmap_image)
+        self.ui.image_label.show()
+    
+
+    def show_chosen_image(self):
+        if self.ui.image_list.currentRow()>=0:
+            self.image_name = self.ui.image_list.currentItem().text()
+            self.load_image(self.image_name)
+            self.show_image()
+
+
+    def save_image(self):
+
+        path = os.path.join(self.workdir, "edited")
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        self.new_path = os.path.join(path, self.image_name)
+        self.new_image.save(self.new_path)
+        self.image_path = self.new_path
+
+
+    def do_black_white(self):
+        self.new_image = self.image.convert("L")
+        self.save_image()
+        self.show_image()
+
+
 
 
 
